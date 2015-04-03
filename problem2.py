1be41545611278a3851b3793dccfceb2667394db
efrainc <efrainc88@gmail.com>
2015-04-02 21:04:04 -0700

    solved problem 2

---
+++ problem2.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+
+def fibtest():
+    i = 0
+    first = 1
+    second = 2
+    total = 2
+    while i < 4000000:
+        i = first + second
+        first = second
+        second = i
+        if i % 2 == 0:
+            total += i
+    return total
+
+
+if __name__ == '__main__':
+    print fibtest()
