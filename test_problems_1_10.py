749521aad6c3be614e2b247355fa2458a08e4826
efrainc <efrainc88@gmail.com>
2015-04-13 14:16:53 -0700

    commented out test 7 for time savings

--- test_problems_1_10.py
+++ test_problems_1_10.py
@@ -37,12 +37,14 @@ def test_problem4():
 
     assert 906609 == probl4()
 
+# This test tackes a long time to run since it has to
+# calculate each prime number from 1 to 10,000. Test passes
+# but commented to out to save time in the future.
+# def test_problem7():
+#     """Testing Problem one edge case
+#     as defined by Project Euler"""
 
-def test_problem7():
-    """Testing Problem one edge case
-    as defined by Project Euler"""
-
-    assert 104743 == find_xprime(10001)
+#     assert 104743 == find_xprime(10001)
 
 
 def test_problem8():
