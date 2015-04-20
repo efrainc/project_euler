784455aacfa2f3c2f25e0cea309c8475c5cf5a97
efrainc <efrainc88@gmail.com>
2015-04-20 16:06:44 -0700

    added findings and notes

--- problem13.py
+++ problem13.py
@@ -2,9 +2,21 @@
 
 
 if __name__ == '__main__':
+    """Pretty Self Explanitory  - but for completeness
+    reads the file line by line. Converts each line into an int
+    and add the int to a cummulative sum"""
+
     a = open('problem13_input.txt', 'r')
     output = 0
     for line in a:
         output += int(line)
 
     print output
+
+
+"""
+Thoughs:
+    Could have saved each item as a variable and then run the sum
+    command. But this would have taken more space.
+
+"""
--- test_problems_11_20.py
+++ test_problems_11_20.py
@@ -10,3 +10,19 @@ def test_problem11():
 
     assert 70600674 == grid_max(grid_creator())
 
+
+"""
+Because of the interesting time complexity of problem 12
+and the choices that could have been made to solve it the testing was
+completed in the python file.
+
+Note:
+    The difference between xrange and while is negligable.
+    Append versus += [] + [] was a little faster - Confirming readings
+    found online.
+    Concatination had no big difference on time. Not idea for reading.
+
+"""
+
+# Because of the simplicity of problem 13 no function formall created
+
